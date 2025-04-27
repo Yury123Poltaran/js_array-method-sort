ba16d0fb3390a9e425d5a82e388c05858f54b4c4
Yury123Poltaran <154461330+Yury123Poltaran@users.noreply.github.com>
2025-04-27 13:16:46 +0300

    Update arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,12 +1,22 @@
-'use strict';
-
-/**
- * Implement method Sort
- */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      for (let j = i + 1; j < this.length; j++) {
+        const result = compareFunction 
+          ? compareFunction(this[i], this[j]) 
+          : String(this[i]).localeCompare(String(this[j]));
+
+        if (result > 0) {
+          // обмін місцями
+          const temp = this[i];
+          this[i] = this[j];
+          this[j] = temp;
+        }
+      }
+    }
+    return this;
   };
 }
 
 module.exports = applyCustomSort;
+
